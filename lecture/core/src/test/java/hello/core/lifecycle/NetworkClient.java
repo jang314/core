b531feaff05442419c98c5a2a282d83e541a7e18
jang314 <jang314@naver.com>
2023-05-21 09:09:06 +0900

    [20230515] 스프링기본 실습 커밋

--- lecture/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ lecture/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,29 +1,43 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
-    private String url;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-    public NetworkClient () {
-        System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-    }
+public class NetworkClient  {
+        private String url;
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+        public NetworkClient () {
+            System.out.println("생성자 호출, url = " + url);
+        }
 
-    public void connect() {
-        System.out.println("connect = " + url);
-    }
+        public void setUrl(String url) {
+            this.url = url;
+        }
 
-    public void call(String message) {
-        System.out.println("call = " + url + " , message = " + message);
-    }
+        public void connect() {
+            System.out.println("connect = " + url);
+        }
 
-    // 서비스 종료 시 호출
-    public void disconnect() {
-        System.out.println("close = " + url);
-    }
+        public void call(String message) {
+            System.out.println("call = " + url + " , message = " + message);
+        }
+
+        // 서비스 종료 시 호출
+        public void disconnect() {
+            System.out.println("close = " + url);
+        }
+
+        public void init()  {
+            System.out.println("NetworkClient.afterPropertiesSet()");
+            connect();
+            call("초기화 연결 메시지");
+        }
+
+        public void close() {
+            System.out.println("NetworkClient.disconnect()");
+            disconnect();
+        }
 }
